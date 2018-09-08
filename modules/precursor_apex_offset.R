a03a65327a7803d1d85dc4e308999f53072a61f7
Albert Chen <blahoink@gmail.com>
2018-09-08 13:29:44 -0400

    add instrument QC plots

---
+++ modules/precursor_apex_offset.R
@@ -0,0 +1,72 @@
+##############################################################################
+## Leave the following code alone:  ##########################################
+##############################################################################
+
+init <- function() {
+  return(list(
+    ##############################################################################
+    ## Define information about the plot: ########################################
+    ##############################################################################
+    
+    # What tab in the sidebar the plot will be added to:
+    tab='Instrument',
+    
+    # Title for the box drawn around the plot
+    boxTitle='Precursor Apex Offset',
+    
+    # Description of the plot and what it accomplishes
+    help='help text for module',
+    
+    ##############################################################################
+    ## Leave the following code alone:  ##########################################
+    ##############################################################################
+    
+    moduleFunc=testModule
+  ))
+}
+
+testModule <- function(input, output, session, data) {
+  
+  ##############################################################################
+  ## Define what MaxQuant data to use:  ########################################
+  ##############################################################################
+  
+  # Options include some of the standard MaxQuant outputs:
+  #   'evidence', 'msms', 'msmsScans', 'allPeptides'
+  data.choice<-'msmsScans'
+  
+  output$plot <- renderPlot({
+    
+    validate(need(data()[[data.choice]],paste0("Upload ", data.choice, '.txt')))
+    #validate(need((input$Exp_Sets),"Loading"))
+    
+    ##############################################################################
+    ## Manipulate your data of choice and plot away!  ############################
+    ##############################################################################
+    
+    # Data that you chose can be called as the variable data.loaded, this an
+    # object of R class 'data frame':
+    
+    data.loaded <- data()[[data.choice]]
+    
+    histdata <- df[,c("Raw.file","Precursor.apex.offset.time")]
+    lengthLev <- length(levels(histdata$Raw.file))
+    histdata$Precursor.apex.offset.time <- histdata$Precursor.apex.offset.time*60
+    histdata$Precursor.apex.offset.time[histdata$Precursor.apex.offset.time > 8] <- 9
+    histdata$Precursor.apex.offset.time[histdata$Precursor.apex.offset.time < -8] <- -9
+    
+    ggplot(histdata, aes(Precursor.apex.offset.time)) + 
+      facet_wrap(~Raw.file, nrow = 1) + 
+      geom_histogram() + 
+      coord_flip() + 
+      theme(panel.background = element_rect(fill = "white",colour = "white"), 
+            panel.grid.major = element_line(size = .25, linetype = "solid",color="lightgrey"), 
+            panel.grid.minor = element_line(size = .25, linetype = "solid",color="lightgrey"),
+            legend.position="none",
+            axis.text.x = element_text(angle = 45, hjust = 1, margin=margin(r=45)), 
+            axis.title=element_text(size=rel(1.2),face="bold"), 
+            axis.text = element_text(size = rel(textVar)),
+            strip.text = element_text(size=rel(textVar))) + 
+      xlab("Apex Offset (sec)")
+  })
+}
---
+++ modules/retention_length_base.R
@@ -0,0 +1,76 @@
+##############################################################################
+## Leave the following code alone:  ##########################################
+##############################################################################
+
+init <- function() {
+  return(list(
+    ##############################################################################
+    ## Define information about the plot: ########################################
+    ##############################################################################
+    
+    # What tab in the sidebar the plot will be added to:
+    tab='Instrument',
+    
+    # Title for the box drawn around the plot
+    boxTitle='Retention Lengths for IDd Ions',
+    
+    # Description of the plot and what it accomplishes
+    help='help text for module',
+    
+    ##############################################################################
+    ## Leave the following code alone:  ##########################################
+    ##############################################################################
+    
+    moduleFunc=testModule
+  ))
+}
+
+testModule <- function(input, output, session, data) {
+  
+  ##############################################################################
+  ## Define what MaxQuant data to use:  ########################################
+  ##############################################################################
+  
+  # Options include some of the standard MaxQuant outputs:
+  #   'evidence', 'msms', 'msmsScans', 'allPeptides'
+  data.choice<-'evidence'
+      
+  output$plot <- renderPlot({
+    
+    validate(need(data()[[data.choice]],paste0("Upload ", data.choice, '.txt')))
+    #validate(need((input$Exp_Sets),"Loading"))
+    
+    ##############################################################################
+    ## Manipulate your data of choice and plot away!  ############################
+    ##############################################################################
+    
+    # Data that you chose can be called as the variable data.loaded, this an
+    # object of R class 'data frame':
+    
+    data.loaded <- data()[[data.choice]]
+    
+    histdata <- data.loaded[,c("Raw.file","Retention.length","PEP")]
+    lengthLev <- length(levels(histdata$Raw.file))
+    #levels(histdata$Raw.file) <- levelsLib[1:lengthLev]
+    histdata$Retention.length <- histdata$Retention.length*60
+    histdata$Retention.length[histdata$Retention.length > 120] <- 120
+    
+    validate(need(nrow(histdata) > 0, 'No data available after filtering'))
+    
+    maxRL <- max(histdata$Retention.length)
+    
+    ggplot(histdata, aes(Retention.length)) + 
+      facet_wrap(~Raw.file, nrow = 1) + 
+      geom_histogram(bins=120) + 
+      coord_flip() + 
+      theme(panel.background = element_rect(fill = "white",colour = "white"), 
+            panel.grid.major = element_line(size = .25, linetype = "solid",color="lightgrey"), 
+            panel.grid.minor = element_line(size = .25, linetype = "solid",color="lightgrey"),
+            legend.position="none", 
+            axis.text.x = element_text(angle = 45,hjust = 1, margin=margin(r=45)), 
+            axis.title=element_text(size=rel(textVar),face="bold"), 
+            axis.text = element_text(size = rel(textVar)),
+            strip.text = element_text(size=rel(textVar))) + 
+      xlab('Retention Lengths at base (sec)')
+  })
+}
