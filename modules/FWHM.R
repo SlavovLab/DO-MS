9fe7a5f4e1989cc99f15e314d1fe594166db8222
hspekt <29742930+hspekt@users.noreply.github.com>
2018-09-08 21:32:12 -0400

    instrument performance tab plots added

---
+++ modules/FWHM.R
@@ -0,0 +1,67 @@
+##############################################################################
+## Leave the following code alone:  ##########################################
+##############################################################################
+
+init <- function() {
+  return(list(
+    
+    ##############################################################################
+    ## Define information about the plot: ########################################
+    ##############################################################################
+    
+    # What tab in the sidebar the plot will be added to:
+    tab='Instrument Performance',
+    
+    # Title for the box drawn around the plot
+    boxTitle='Elution profile: FWHM',
+    
+    # Description of the plot and what it accomplishes
+    help='Plotting the distrution of elution profile widths at half the maximum
+    intensity value for each peak.',
+    
+    ##############################################################################
+    ## Leave the following code alone:  ##########################################
+    ##############################################################################
+    
+    moduleFunc=testModule
+    
+    
+  ))
+}
+
+testModule <- function(input, output, session, data) {
+  
+  output$plot <- renderPlot({
+    
+    ##############################################################################
+    ## Define what MaxQuant data to use, manipulate that data, and plot:  ########
+    ##############################################################################
+    
+    # Options include some of the standard MaxQuant outputs:
+    #   'evidence', 'msms', 'msmsScans', 'allPeptides'
+    data.choice<-'allPeptides'
+    
+    ##############################################################################
+    ## Leave the following code alone:  ##########################################
+    ##############################################################################
+    
+    validate(need(data()[[data.choice]],paste0("Upload ", data.choice,".txt")))
+    #validate(need((length(input$Exp_Sets) == 1),"Please select a single experiment"))
+    
+    ##############################################################################
+    ## Manipulate your data of choice and plot away!  ############################
+    ##############################################################################
+    
+    # Data that you chose can be called as the variable data.loaded, this an
+    # object of R class 'data frame':
+    data.loaded <- data()[[data.choice]]
+    
+    # Plot:
+    histdata <- data.loaded[,c("Raw.file","Retention.length..FWHM.")]
+    histdata$Retention.length..FWHM.[histdata$Retention.length..FWHM. > 45] <- 49
+    ggplot(histdata, aes(Retention.length..FWHM.)) + facet_wrap(~Raw.file, nrow = 1)+ geom_histogram(bins = 49) + coord_flip() + theme(panel.background = element_rect(fill = "white",colour = "white"), panel.grid.major = element_line(size = .25, linetype = "solid",color="lightgrey"), panel.grid.minor = element_line(size = .25, linetype = "solid",color="lightgrey"),legend.position="none",axis.text.x = element_text(angle = 45, hjust = 1, margin=margin(r=45)),axis.title=element_text(size=rel(1.2),face="bold"), axis.text = element_text(size = rel(textVar)),strip.text = element_text(size=rel(textVar))) + xlab("Retention Length FWHM (sec)")
+    
+    })
+  
+}
+
