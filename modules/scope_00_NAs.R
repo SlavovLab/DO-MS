becbeff55580f15bc0bbaec811c34d327a92b622
Albert Chen <blahoink@gmail.com>
2018-12-05 16:53:11 -0500

    shorten x-axis RI labels

--- modules/scope_00_NAs.R
+++ modules/scope_00_NAs.R
@@ -16,6 +16,13 @@ init <- function() {
     
     RI<-colnames(ev)[grep("Reporter.intensity.corrected.", colnames(ev))]
     
+    # make RI channel names shorter -- easier to plot
+    ri_col_names <- colnames(ev)[grep('Reporter.intensity.corrected.', colnames(ev))]
+    new_ri_col_names <- gsub('Reporter\\.intensity\\.corrected\\.', 'RI_', ri_col_names)
+    colnames(ev)[grep('Reporter.intensity.corrected.', colnames(ev))] <- new_ri_col_names
+    RI <- new_ri_col_names
+      
+    
     nraw<-length(unique(ev$Raw.file))
     
     na.mat<-matrix(data = NA, nrow = nraw, ncol = length(RI))
@@ -43,11 +50,9 @@ init <- function() {
     plotdata <- .plotdata(data, input)
     
     rgb.palette <- colorRampPalette(c("blue", "yellow"), space = "rgb")
-    levelplot(t(plotdata), aspect="iso", main="Missing data per TMT Channel", scales=list(x=list(rot=45)), xlab="", xaxt='n', ylab="", 
+    levelplot(t(plotdata), aspect="iso", main="Missing data per TMT Channel", 
+              scales=list(x=list(rot=45)), xlab="", xaxt='n', ylab="", 
               col.regions=rgb.palette(120), cuts=100, at=seq(0,1,0.01))
-    
-    
-    
     }
   
   return(list(
