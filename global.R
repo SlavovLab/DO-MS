6fc4594d91bb3cbe4068461e3e799561c51c6e85
Albert Chen <blahoink@gmail.com>
2018-12-06 14:23:03 -0500

    add get_os helper func

--- global.R
+++ global.R
@@ -161,3 +161,15 @@ downloadButtonFixed <- function(outputId, label = "Download", class = NULL, ...)
       ...
     )
 }
+
+get_os <- function() {
+  if (.Platform$OS.type == "windows") { 
+    "win"
+  } else if (Sys.info()["sysname"] == "Darwin") {
+    "mac" 
+  } else if (.Platform$OS.type == "unix") { 
+    "unix"
+  } else {
+    stop("Unknown OS")
+  }
+}
