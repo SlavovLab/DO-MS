7ffce1db261ec09ec7cbee14fd5f2e56cc843fc6
Albert Chen <blahoink@gmail.com>
2019-01-30 17:23:05 -0500

    cleanup

--- resources/app.js
+++ resources/app.js
@@ -3,15 +3,6 @@ $(document).ready(function() {
   console.log('ready!');
   $('body').addClass('fixed');
   
-  // for the select all/select none buttons
-
-  $('#exp_check_all').click(function() {
-    $('input[name="Exp_Sets"]').prop("checked", true);
-  });
-  $('#exp_check_none').click(function() {
-    $('input[name="Exp_Sets"]').removeAttr("checked");
-  });
-  
   // select all/select none in input files
   
   $('#files_check_all').click(function() {
@@ -33,29 +24,6 @@ $(document).ready(function() {
   $(function () {
     $('[data-toggle="popover"]').popover()
   })
-
-  /*var a = document.getElementById('fileIn');
-  if(a.value === "") {
-      noFile.innerHTML = "No folder choosen";
-  } else {
-      noFile.innerHTML = "";
-  }
-  
-  document.getElementById("fileIn").addEventListener("change", function(e) {
-
-    let files = e.target.files;
-    var arr = new Array(files.length*2);
-    for (let i=0; i<files.length; i++) {
-      //console.log(files[i])
-      console.log(files[i].webkitRelativePath);
-      //console.log(files[i].name);
-      arr[i] = files[i].webkitRelativePath;
-      arr[i+files.length] = files[i].name;
-    }
-
-    //Shiny.onInputChange("mydata", arr);
-
-  });*/
 
 });
 
