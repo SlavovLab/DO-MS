f006834cc01549ee1928b3cf97117fb804b5f195
Gray Huffman <40698035+GrayHuffman@users.noreply.github.com>
2019-03-05 15:37:18 -0500

    [Reuploading intersected module]

---
+++ modules/020_Ion_Sampling/instrument_02_MS1_Intersected_int.R
@@ -0,0 +1,72 @@
+init <- function() {
+  
+  type <- 'plot'
+  box_title <- 'MS1 Intensity for identified ions in common'
+  help_text <- 'Plotting the MS1 intensity for all common identified ions across runs.'
+  source_file <- 'evidence'
+  
+  .validate <- function(data, input) {
+    validate(need(data()[['evidence']], paste0('Upload evidence.txt')))
+  }
+  
+  .plotdata <- function(data, input) {
+    plotdata <- data()[['evidence']][,c('Raw.file', 'Intensity')]
+    plotdata$Intensity <- log10(plotdata$Intensity)
+    plotdata$SeqCharge <- paste0(plotdata$Sequence,"_",plotdata$Charge)
+    
+    # Thresholding data at 1 and 99th percentiles
+    ceiling <- quantile(plotdata$Intensity, probs=.99, na.rm = TRUE)
+    floor <- quantile(plotdata$Intensity, probs=.01, na.rm = TRUE)
+    
+    plotdata <- dplyr::filter(plotdata, is.finite(Intensity))
+    
+    plotdata[plotdata$Intensity >= ceiling, 2] <- ceiling
+    plotdata[plotdata$Intensity <= floor, 2] <- floor
+    
+    #Assemble list of peptides in each Raw file
+    plotdata$Raw.file <- factor(plotdata$Raw.file)
+    expsInDF <- levels(plotdata$Raw.file)
+    peplist <- list()
+    for (i in 1:length(expsInDF)){
+      peptidesDF <- dplyr::filter(plotdata, Raw.file == expsInDF[i])
+      peptides <- dplyr::select(peptidesDF, SeqCharge)
+      peplist[[i]] <- peptides
+    }
+    
+    #Get intersection of all peptides
+    intersectList <- as.vector(Reduce(intersect, peplist))
+    
+    #Get reduced dataframe for elements that match intersection
+    plotdata_Intersected <- dplyr::filter(plotdata, SeqCharge %in% intersectList$SeqCharge)
+    
+    plotdata <- plotdata_Intersected
+    
+    return(plotdata)
+  }
+  
+  .plot <- function(data, input) {
+    .validate(data, input)
+    plotdata <- .plotdata(data, input)
+    
+
+    
+    ggplot(plotdata_Intersected, aes(Intensity)) + 
+      facet_wrap(~Raw.file, nrow = 1) + 
+      geom_histogram(bins=100) + 
+      coord_flip() + 
+      labs(x=expression(bold('Log'[10]*' Precursor Intensity')), y='Number of Peptides') +
+      theme_base(input=input)
+  }
+  
+  return(list(
+    type=type,
+    box_title=box_title,
+    help_text=help_text,
+    source_file=source_file,
+    validate_func=.validate,
+    plotdata_func=.plotdata,
+    plot_func=.plot,
+    dynamic_width=150,
+    dynamic_width_base=150
+  ))
+}
