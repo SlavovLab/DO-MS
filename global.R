8730c7fd4753d3ff31ddf6d62a236272d1d84221
hspekt <hms89@cornell.edu>
2021-03-08 12:43:13 -0500

    Not requiring R version > 4.0.0 for now, may reimplement if issues

--- global.R
+++ global.R
@@ -1,9 +1,9 @@
 version <- '1.0.10'
 
 # check R version. required R >= 3.5.0
-if(as.numeric(R.Version()$major) < 4) {
-  stop('R Version >= 4.0.0 required. Download the latest version of R from the CRAN page: https://cran.r-project.org/')
-}
+# if(as.numeric(R.Version()$major) < 4) {
+#   stop('R Version >= 4.0.0 required. Download the latest version of R from the CRAN page: https://cran.r-project.org/')
+# }
 
 # first, get pacman
 if(!'pacman' %in% installed.packages()[,'Package']) {
